8660522c57690c596fd0b59e2d60e163a81afb11
Nandrianina Hery <herynandrianina03@gmail.com>
2025-04-01 14:45:19 +0300

    Clean main

--- backend/config.py
+++
@@ -1,4 +0,0 @@
-class Config:
-    SECRET_KEY = 'votre_cle_secrete'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://username:password@localhost:5432/votre_base_de_donnees'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
