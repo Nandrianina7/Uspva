036b900fab4caf871445baa2109d916a1059a8b1
Natou <natouayo@gmail.com>
2025-04-07 15:44:55 +0300

    params

--- backend/app.py
+++ backend/app.py
@@ -1,22 +1,18 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from models import db, User, File, Classe, Student, Matiere, Note, ClasseMatiere, TimeTable, Event
+from .models import db, User, File, Classe, Student, Matiere, Note, ClasseMatiere, TimeTable, Event
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 app = Flask(__name__)
-DATABASE_URL = os.getenv('DATA_BASE_URL')
-with app.app_context():
-    db.create_all()
 
-# Configuration de la connexion PostgreSQL
-# natou = anarana user any amin postgresql
-# root = tenimiafina
-# localhost = ip an'ny serveur
-# 5432 = port an'ny serveur
-# usvpa = anarana database
+# Configurations AVANT l'initialisation de la DB
+DATABASE_URL = os.getenv('DATA_BASE_URL')
 app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
