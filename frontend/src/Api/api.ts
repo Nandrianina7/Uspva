bd147a6b071819e756571d99349b22b680e75425
Nandrianina Hery <herynandrianina03@gmail.com>
2025-04-10 14:21:00 +0300

    fixed token refresh

--- frontend/src/Api/api.ts
+++ frontend/src/Api/api.ts
@@ -1,41 +1,37 @@
-import axios from "axios";
+import axios, { AxiosResponse } from 'axios';
 
 const api = axios.create({
   baseURL: 'http://localhost:5000/api',
-  withCredentials: true
+  withCredentials: true,
+  timeout: 1000,
 });
 
-api.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem('access_token');
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`
+api.interceptors.response.use(
+  (response: AxiosResponse) => response,
+  async (error: any) => {
+    const originalRequest = error.config;
+
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      console.warn('Token expired. Attempting refresh...');
+
+      try {
+        await axios.post(
+          'http://localhost:5000/api/refresh',
+          {},
+          { withCredentials: true },
+        );
+        console.log('Token refreshed');
+        return api(originalRequest);
+      } catch (err) {
+        console.error('Refresh failed', err);
+        window.location.href = '/';
+        return Promise.reject(err);
+      }
     }
-    return config
+
+    return Promise.reject(error);
   },
-  (error) => Promise.reject(error)
 );
 
-api.interceptors.response.use(
-  (response) => response,
-  async (error) => {
-    const originalReq = error.config;
-    if (error.response?.status === 401 && !originalReq._retry) {
-      originalReq._retry = true;
-      try {
-        const {data} = await axios.post('http://localhost/api/refresh', {}, {withCredentials: true});
-        localStorage.setItem('access_token', data.access_token);
-        // originalReq.headers.Authorization = `Bearer ${data.access_token}`;
-        return api(originalReq);
-      }
-      catch (error){
-        localStorage.removeItem('access_token');
-        window.location.href = ('/')
-        return Promise.reject(error)
-      }
-    }
-    return Promise.reject(error)
-
-  }
-)
 export default api;
